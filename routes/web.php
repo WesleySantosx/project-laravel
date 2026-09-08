43a2bf1cb120cfd053dff541992ea41cbcbe10a3
Wesley <wesleys.c190@gmail.com>
2026-09-08 18:15:39 -0300

    feat: atv01

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('sobre', function (){
+    return "sobre"; 
+});
+Route::get('alunos', function (){
+    return "alunos"; 
+});
+Route::get('contatos', function (){
+    return "contatos"; 
+});
