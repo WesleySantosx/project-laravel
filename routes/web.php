15af3993eea41a51b8c21ef30874bdec48f328c2
Wesley <wesleys.c190@gmail.com>
2026-09-08 18:41:16 -0300

    feat: atv3

--- routes/web.php
+++ routes/web.php
@@ -5,22 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('sobre', function (){
-    return "sobre"; 
+Route::get('sobre', function () {
+    return "sobre";
 });
-Route::get('alunos', function (){
-    return "alunos"; 
+Route::get('alunos', function () {
+    return "alunos";
 });
-Route::get('contatos', function (){
-    return "contatos"; 
+Route::get('contatos', function () {
+    return "contatos";
 });
 
-Route::get('produto{id}', function ($id){
-    return "produto: {$id}"; 
+Route::get('produto{id}', function ($id) {
+    return "produto: {$id}";
 });
-Route::get('usuario{id}', function ($id){
-    return "usuario: {$id}"; 
+Route::get('usuario{id}', function ($id) {
+    return "usuario: {$id}";
 });
-Route::get('categoria{id}', function ($id){
-    return "categoria: {$id}"; 
+Route::get('categoria{id}', function ($id) {
+    return "categoria: {$id}";
 });
