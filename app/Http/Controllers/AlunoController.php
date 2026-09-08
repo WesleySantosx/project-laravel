f572a50eac3bd39a56deb2b8cf852580fb9f1f5f
Wesley <wesleys.c190@gmail.com>
2026-09-08 18:48:09 -0300

    feat: atv4

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
+    public function index()
+    {
+        return "Listando todos os alunos";
+    }
+    public function create()
+    {
+        return "Formulário de cadastro de aluno";
+    }
+    public function store(Request $request)
+    {
+        return "Salvando novo aluno";
+    }
+    public function show($id)
+    {
+        return "Exibindo detalhes do aluno com ID: {$id}";
+    }
+    public function edit($id)
+    {
+        return "Formulário de edição do aluno com ID: {$id}";
+    }
+    public function update(Request $request, $id)
+    {
+        return "Atualizando aluno com ID: {$id}";
+    }
+    public function destroy($id)
+    {
+        return "Excluindo aluno com ID: {$id}";
+    }
     
 }
