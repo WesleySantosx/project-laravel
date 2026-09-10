1a68fa598c446d1798001bee778379cda6078663
Wesley <wesleys.c190@gmail.com>
2026-09-09 22:03:38 -0300

    feat: atv17

--- database/migrations/2026_09_09_235354_crate_aluno_table.php
+++ database/migrations/2026_09_09_235354_crate_aluno_table.php
@@ -11,7 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('alunos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->string('curso');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('alunos');
     }
 };
